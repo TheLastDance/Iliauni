402c43488240b4f5b9acccecd8d41b777ac49fd3
unknown <davit.khachaturov.1@iliauni.edu.ge>
2023-11-02 23:43:48 +0400

    implemented Landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Landing />} ></Route>
+      </Routes>
     </>
   )
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,19 @@
 import './Landing.css'
+import { Link } from 'react-router-dom';
+import notepad_img from "../../assets/notepad.png"
 
 function Landing() {
 
   return (
-    <>
-    </>
+    <main>
+      <section className='landing'>
+        <div className='landing_container'>
+          <img src={notepad_img} alt="notepad image" />
+          <h1>Get Started Today</h1>
+          <Link to='/auth'><button>Get Started</button></Link>
+        </div>
+      </section>
+    </main>
   )
 }
 
